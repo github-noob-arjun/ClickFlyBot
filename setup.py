1438b7aa721b30c642d416d02c40eeb340e23f39
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-20 09:02:42 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Short',
+      version='0.1.1',
+      description='Python module for interacting with Chatbase APIs',
+      url='https://github.com/github-noob-arjun/ClickFlyBot',
+      author='Github',
+      author_email='None',
+      license='MIT Pro',
+      packages=['Short'],      
+      zip_safe=False)
